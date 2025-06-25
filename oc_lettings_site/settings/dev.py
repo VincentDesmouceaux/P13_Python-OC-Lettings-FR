6315f2c12dcd0c39d602765bd2e37eacec98318f
VincentDesmouceaux <desmontvincent@gmail.com>
2025-06-25 14:32:52 +0200

    fake8 ready

--- oc_lettings_site/settings/dev.py
+++ oc_lettings_site/settings/dev.py
@@ -1,13 +1,3 @@
-from .base import *
-
-DEBUG = True
-
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    }
-}
 from .base import *  # noqa: F401,F403
 
 DEBUG = True
--- oc_lettings_site/settings/prod.py
+++ oc_lettings_site/settings/prod.py
@@ -1,8 +1,8 @@
-from .base import *  # noqa: F401,F403
+from .base import *  # noqa: F401,F403,F405
 
 DEBUG = False
 
-# la variable MIDDLEWARE vient du `import *`
+# la variable MIDDLEWARE provient du « import * »
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 
 STORAGES = {
